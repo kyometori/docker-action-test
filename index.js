e5d0e6fbe32aefe698d1aad7d57511062c54b101
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2025-01-25 03:41:33 +0800

    chore: back to use username

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const is_odd = require('is-odd')
 
+console.log("====================================")
+console.log("Program Begins")
 console.log(is_odd(1)) // true
+console.log(is_odd(2)) // false
+console.log(is_odd(3)) // true
+console.log("Program Ends")
+console.log("====================================")
